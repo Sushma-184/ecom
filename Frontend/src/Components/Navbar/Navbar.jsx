7d763af78103068fd6876646c36e3beee36994d1
Sushma Korra <korrasushma123@gmail.com>
2024-12-17 17:18:37 +0530

    Update Navbar.jsx

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar=()=>{
     <div className='navbar'>
         <div className='nav-logo'>
             <img src={logo} alt="logo" />
-             <p>SHOPPER</p>
+             <p>SNAPCART</p>
         </div>
         <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="dropdown" />
         <ul ref={menuRef} className='nav-menu' >
